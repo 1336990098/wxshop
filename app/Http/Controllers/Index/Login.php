b962b3d8050cf5b916df770b820f9a1b13a46174
1336990098 <1336990098@qq.com>
2019-03-23 14:33:48 +0800

    a new shop

--- app/Http/Controllers/Index/Login.php
+++ app/Http/Controllers/Index/Login.php
@@ -107,34 +107,34 @@ class Login extends Controller
      * @params  $mobile  要发送的手机号
      *
      * */
-//    private function sendMobile($mobile)
-//    {
-//        $host = env("MOBILE_HOST");
-//        $path = env("MOBILE_PATH");
-//        $method = "POST";
-//        $appcode = env("MOBILE_APPCODE");
-//        $headers = array();
-//        $code = $this->createcode(4);
-//        session(['mobilecode'=>$code]);
-//        array_push($headers, "Authorization:APPCODE " . $appcode);
-//        $querys = "content=【创信】你的验证码是："."$code"."，3分钟内有效！&mobile=".$mobile;
-//        $bodys = "";
-//        $url = $host . $path . "?" . $querys;
-//
-//        $curl = curl_init();
-//        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-//        curl_setopt($curl, CURLOPT_URL, $url);
-//        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-//        curl_setopt($curl, CURLOPT_FAILONERROR, false);
-//        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//        curl_setopt($curl, CURLOPT_HEADER, true);
-//        if (1 == strpos("$".$host, "https://"))
-//        {
-//            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-//            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-//        }
-//        return curl_exec($curl);
-//    }
+    private function sendMobile($mobile)
+    {
+        $host = env("MOBILE_HOST");
+        $path = env("MOBILE_PATH");
+        $method = "POST";
+        $appcode = env("MOBILE_APPCODE");
+        $headers = array();
+        $code = $this->createcode(4);
+        session(['mobilecode'=>$code]);
+        array_push($headers, "Authorization:APPCODE " . $appcode);
+        $querys = "content=【创信】你的验证码是："."$code"."，3分钟内有效！&mobile=".$mobile;
+        $bodys = "";
+        $url = $host . $path . "?" . $querys;
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_FAILONERROR, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, true);
+        if (1 == strpos("$".$host, "https://"))
+        {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        }
+        return curl_exec($curl);
+    }
 
 
 }
